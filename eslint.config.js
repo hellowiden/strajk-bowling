8d11a2b206c7b6977d0c4d0d883eb331461ab4b7
Marcus Widen <hellowiden@gmail.com>
2024-12-09 16:53:17 +0100

    jest

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import pluginJest from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-
   pluginJs.configs.recommended,
 
   // React configuration
@@ -33,6 +33,27 @@ export default [
       "react/react-in-jsx-scope": "off", 
       "react/prop-types": "off", 
       "react/no-unescaped-entities": "off", 
+    },
+  },
+
+  // Jest configuration
+  {
+    files: ["**/*.{test,spec}.{js,mjs,cjs,jsx}"], // Target test files
+    languageOptions: {
+      globals: {
+        ...globals.node, // Node.js globals for test files
+        ...globals.jest, // Jest globals
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      jest: pluginJest,
+    },
+    rules: {
+      ...pluginJest.configs.recommended.rules, // Apply recommended Jest rules
     },
   },
 ];
