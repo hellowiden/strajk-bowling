44344ddc0bb13e2d41e68cfaa6e15bd76f3884e1
Marcus Widen <hellowiden@gmail.com>
2024-12-29 16:18:49 +0100

    upd jest.config file

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,11 @@
 module.exports = {
-    "testEnvironment": "jsdom",
-    "transform": {
-        "^.+\\.[t|j]sx?$": "babel-jest",
-        "^.+\\.svg$": "jest-transformer-svg"
-    },
-    "transformIgnorePatterns": ['/node_modules/(?!(nanoid)/)'],
-}
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+    '^.+\\.svg$': 'jest-transformer-svg',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(nanoid)/)'],
+  collectCoverage: true, // Enable coverage collection
+  coverageDirectory: './coverage', // Output coverage files to the "coverage" directory
+  coverageReporters: ['json', 'text', 'lcov'], // Generate JSON, text, and LCOV reports
+};
