2e68dc7a2c64e838f8a5734669f8fa2fb60f63f4
Marcus Widen <hellowiden@gmail.com>
2024-12-29 16:24:11 +0100

    upd workflow main

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,8 +5,4 @@ module.exports = {
     '^.+\\.svg$': 'jest-transformer-svg',
   },
   transformIgnorePatterns: ['/node_modules/(?!(nanoid)/)'],
-  collectCoverage: true, // Enable coverage collection
-  coverageDirectory: './coverage', // Output coverage files to the "coverage" directory
-  coverageReporters: ['json', 'text', 'lcov'], // Generate JSON, text, and LCOV reports
-  globalTeardown: './scripts/createCoverageReport.js',
 };
